5231b7750279f780703bacbf361bc54240723d86
ts-manuel <ts.manuel.dev@gmail.com>
2021-12-18 13:50:37 +0100

    Added decoder implementation

---
+++ hdl/rtl/core/decoder.vhd
@@ -0,0 +1,186 @@
+-- *********************************************************************
+-- File: decoder.vhd
+--
+-- CPU Instruction Decoder
+--
+-- Generic:
+--
+-- Port:
+--  i_clk:          clock input
+--  i_ce:           clock enable active HIGH
+--  i_opcode:       instruction to decode
+--  o_rs1:          source register 1
+--  o_rs2:          source register 2
+--  o_rd:           destination register
+--  o_reg_mux:      register input multiplexer
+--  o_reg_wb:       enable register write back
+--  o_imm:          immediate value
+--  o_alu_in1_mux:  alu input 1 multiplexer
+--  o_alu_in2_mux:  alu input 2 multiplexer
+--  o_alu_func:     alu function
+--  o_branch_func:  branch condition
+--  o_branch:       this is a branch instruction
+--  o_jump:         this is a jump instruction
+--  o_byte_enable:  byte enable for memory write
+--  o_load_unsign:  when active loads from memory are not sign extended
+--  o_load:         this is a load instruction
+--  o_store:        this is a store instruction
+--
+-- *********************************************************************
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity decoder is
+  port (
+    i_clk         : in  std_logic;
+    i_ce          : in  std_logic;
+    i_opcode      : in  std_logic_vector(31 downto 0);
+    o_rs1         : out std_logic_vector( 4 downto 0);
+    o_rs2         : out std_logic_vector( 4 downto 0);
+    o_rd          : out std_logic_vector( 4 downto 0);
+    o_reg_mux     : out std_logic_vector( 1 downto 0);
+    o_reg_wb      : out std_logic;
+    o_imm         : out std_logic_vector(31 downto 0);
+    o_alu_in1_mux : out std_logic;
+    o_alu_in2_mux : out std_logic;
+    o_alu_func    : out std_logic_vector(4 downto 0);
+    o_branch_func : out std_logic_vector(2 downto 0);
+    o_branch      : out std_logic;
+    o_jump        : out std_logic;
+    o_byte_enable : out std_logic_vector(3 downto 0);
+    o_load_unsign : out std_logic;
+    o_load        : out std_logic;
+    o_store       : out std_logic
+  );
+end entity decoder;
+
+
+architecture behave of decoder is
+
+  type e_type is (ALU_REG, ALU_IMM, LOAD, JALR, STORE, BRANCH, LUI, AUIPC, JAL, ILLEGAL);
+  signal w_ins_type : e_type;
+
+
+  signal w_rs1         : std_logic_vector( 4 downto 0);
+  signal w_rs2         : std_logic_vector( 4 downto 0);
+  signal w_rd          : std_logic_vector( 4 downto 0);
+  signal w_reg_mux     : std_logic_vector( 1 downto 0);
+  signal w_reg_wb      : std_logic;
+  signal w_imm         : std_logic_vector(31 downto 0);
+  signal w_alu_in1_mux : std_logic;
+  signal w_alu_in2_mux : std_logic;
+  signal w_alu_func    : std_logic_vector(4 downto 0);
+  signal w_branch_func : std_logic_vector(2 downto 0);
+  signal w_branch      : std_logic;
+  signal w_jump        : std_logic;
+  signal w_byte_enable : std_logic_vector(3 downto 0);
+  signal w_load_unsign : std_logic;
+  signal w_load        : std_logic;
+  signal w_store       : std_logic;
+
+begin
+
+  -- Decode instruction type
+  with i_opcode(6 downto 2) select
+    w_ins_type <= ALU_REG when "01100",
+                  ALU_IMM when "00100",
+                  LOAD    when "00000",
+                  JALR    when "11001",
+                  STORE   when "01000",
+                  BRANCH  when "11000",
+                  LUI     when "01101",
+                  AUIPC   when "00101",
+                  JAL     when "11011",
+                  ILLEGAL when others;
+
+  -- Signals straight from the opcode
+  w_rs1         <= i_opcode(19 downto 15);
+  w_rs2         <= i_opcode(24 downto 20);
+  w_rd          <= i_opcode(11 downto 7);
+  w_branch_func <= i_opcode(14 downto 12);
+
+  -- ALU control signals
+  w_alu_func(3)           <= '1' when (i_opcode(30) = '1' and (w_ins_type = ALU_REG or i_opcode(14 downto 12) = "101")) else '0';
+  w_alu_func(2 downto 0)  <= i_opcode(14 downto 12) when (w_ins_type = ALU_REG or w_ins_type = ALU_IMM) else "000";
+
+  -- Control signals
+  w_reg_mux     <= "01" when (w_ins_type = LOAD) else
+                   "10" when (w_ins_type = JAL)  else
+                   "10" when (w_ins_type = JALR) else
+                   "00";
+  w_reg_wb      <= '0'  when (w_ins_type = STORE or w_ins_type = BRANCH)  else '1';
+  w_alu_in1_mux <= '1'  when (w_ins_type = AUIPC or w_ins_type = JAL or w_ins_type = BRANCH) else '0';
+  w_alu_in2_mux <= '0'  when (w_ins_type = ALU_REG) else '1';
+  w_alu_func(4) <= '1'  when (w_ins_type = LUI)     else '0';
+  w_branch      <= '1'  when (w_ins_type = BRANCH)  else '0';
+  w_jump        <= '1'  when (w_ins_type = JALR or w_ins_type = JAL) else '0';
+  w_load        <= '1'  when (w_ins_type = LOAD)  else '0';
+  w_store       <= '1'  when (w_ins_type = STORE) else '0';
+
+
+  with i_opcode(13 downto 12) select
+    w_byte_enable <=  "0001" when "00",
+                      "0011" when "01",
+                      "1111" when "10",
+                      "0000" when others;
+                
+  w_load_unsign <= i_opcode(14);
+
+
+  -- Decode immediate
+  p_imm : process (i_opcode, w_ins_type)
+  begin
+    if (w_ins_type = JALR or w_ins_type = LOAD or w_ins_type = ALU_IMM) then
+      -- I-TYPE
+      w_imm(11 downto  0) <= i_opcode(31 downto 20);
+      w_imm(31 downto 12) <= (others=>i_opcode(31));
+    elsif (w_ins_type = STORE) then
+      -- S-TYPE
+      w_imm(11 downto  0) <= i_opcode(31 downto 25) & i_opcode(11 downto 7);
+      w_imm(31 downto 12) <= (others=>i_opcode(31));
+    elsif (w_ins_type = BRANCH) then
+      -- B-TYPE
+      w_imm(11 downto  0) <= i_opcode(7) & i_opcode(30 downto 25) & i_opcode(11 downto 8) & '0';
+      w_imm(31 downto 12) <= (others=>i_opcode(31));
+    elsif (w_ins_type = LUI or w_ins_type = AUIPC) then
+      -- U-TYPE
+      w_imm(11 downto  0) <= (others=>'0');
+      w_imm(31 downto 12) <= i_opcode(31 downto 12);
+    else
+      -- J-TYPE
+      w_imm(19 downto  0) <= i_opcode(19 downto 12) & i_opcode(20) & i_opcode(30 downto 21) & '0';
+      w_imm(31 downto 20) <= (others=>i_opcode(31));
+    end if;
+  end process p_imm;
+  
+
+  -- Update outputs on i_clk rising edge when the decoder is enabled
+  p_out : process (i_clk)
+  begin
+    if (rising_edge(i_clk)) then
+      if (i_ce = '1') then
+        o_rs1         <= w_rs1;        
+        o_rs2         <= w_rs2;        
+        o_rd          <= w_rd;         
+        o_reg_mux     <= w_reg_mux;    
+        o_reg_wb      <= w_reg_wb;     
+        o_imm         <= w_imm;        
+        o_alu_in1_mux <= w_alu_in1_mux;
+        o_alu_in2_mux <= w_alu_in2_mux;
+        o_alu_func    <= w_alu_func;   
+        o_branch_func <= w_branch_func;
+        o_branch      <= w_branch;     
+        o_jump        <= w_jump;       
+        o_byte_enable <= w_byte_enable;
+        o_load_unsign <= w_load_unsign;
+        o_load        <= w_load;       
+        o_store       <= w_store;      
+      end if;
+    end if;
+  end process p_out;
+
+end architecture behave;
---
+++ hdl/tb/core/decoder_tb.vhd
@@ -0,0 +1,269 @@
+-- *********************************************************************
+-- File: decoder_tb.vhd
+--
+-- Control Logic Testbench
+--
+-- *********************************************************************
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_textio.all;
+use std.textio.all; 
+use std.env.finish;
+
+
+entity decoder_tb is
+end entity;
+
+
+architecture behave of decoder_tb is
+
+  constant CLK_P : time := 10 ns;
+
+  signal r_clk          : std_logic := '0';
+  signal r_ce           : std_logic;
+  signal r_opcode       : std_logic_vector(31 downto 0);
+  signal w_rs1          : std_logic_vector( 4 downto 0);
+  signal w_rs2          : std_logic_vector( 4 downto 0);
+  signal w_rd           : std_logic_vector( 4 downto 0);
+  signal w_reg_mux      : std_logic_vector( 1 downto 0);
+  signal w_reg_wb       : std_logic;
+  signal w_imm          : std_logic_vector(31 downto 0);
+  signal w_alu_in1_mux  : std_logic;
+  signal w_alu_in2_mux  : std_logic;
+  signal w_alu_func     : std_logic_vector(4 downto 0);
+  signal w_branch_func  : std_logic_vector(2 downto 0);
+  signal w_branch       : std_logic;
+  signal w_jump         : std_logic;
+  signal w_byte_enable  : std_logic_vector(3 downto 0);
+  signal w_load_unsign  : std_logic;
+  signal w_load         : std_logic;
+  signal w_store        : std_logic;
+
+
+  -- Test patters to apply
+  type pattern_type is record
+    opcode      : std_logic_vector(31 downto 0);
+    rs1         : std_logic_vector(4 downto 0);
+    rs2         : std_logic_vector(4  downto 0);
+    rd          : std_logic_vector(4 downto 0);
+    reg_mux     : std_logic_vector(1 downto 0);
+    reg_wb      : std_logic;
+    imm         : std_logic_vector(31 downto 0);
+    alu_in1_mux : std_logic;
+    alu_in2_mux : std_logic;
+    alu_func    : std_logic_vector(4 downto 0);
+    branch_func : std_logic_vector(2 downto 0);
+    branch      : std_logic;
+    jump        : std_logic;
+    byte_enable : std_logic_vector(3 downto 0);
+    load_unsign : std_logic;
+    load        : std_logic;
+    store       : std_logic;
+  end record;
+  type pattern_array is array (natural range <>) of pattern_type;
+  constant patterns : pattern_array := (
+    (x"000000b7", "-----", "-----", "00001", "00", '1', x"00000000", '-', '1', "1----", "---", '0', '0', "----", '-', '0', '0'),  -- LUI
+    (x"00001137", "-----", "-----", "00010", "00", '1', x"00001000", '-', '1', "1----", "---", '0', '0', "----", '-', '0', '0'),
+    (x"80000237", "-----", "-----", "00100", "00", '1', x"80000000", '-', '1', "1----", "---", '0', '0', "----", '-', '0', '0'),
+    (x"0aa55437", "-----", "-----", "01000", "00", '1', x"0aa55000", '-', '1', "1----", "---", '0', '0', "----", '-', '0', '0'),
+    (x"12345837", "-----", "-----", "10000", "00", '1', x"12345000", '-', '1', "1----", "---", '0', '0', "----", '-', '0', '0'),
+    (x"ffffffb7", "-----", "-----", "11111", "00", '1', x"fffff000", '-', '1', "1----", "---", '0', '0', "----", '-', '0', '0'),
+
+    (x"00000097", "-----", "-----", "00001", "00", '1', x"00000000", '1', '1', "00000", "---", '0', '0', "----", '-', '0', '0'),  -- AUIPC
+    (x"00001117", "-----", "-----", "00010", "00", '1', x"00001000", '1', '1', "00000", "---", '0', '0', "----", '-', '0', '0'),
+    (x"80000217", "-----", "-----", "00100", "00", '1', x"80000000", '1', '1', "00000", "---", '0', '0', "----", '-', '0', '0'),
+    (x"0aa55417", "-----", "-----", "01000", "00", '1', x"0aa55000", '1', '1', "00000", "---", '0', '0', "----", '-', '0', '0'),
+    (x"12345817", "-----", "-----", "10000", "00", '1', x"12345000", '1', '1', "00000", "---", '0', '0', "----", '-', '0', '0'),
+    (x"ffffff97", "-----", "-----", "11111", "00", '1', x"fffff000", '1', '1', "00000", "---", '0', '0', "----", '-', '0', '0'),
+
+    (x"000001ef", "-----", "-----", "00011", "10", '1', x"00000000", '1', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),  -- JAL
+    (x"002002ef", "-----", "-----", "00101", "10", '1', x"00000002", '1', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),
+    (x"fffff4ef", "-----", "-----", "01001", "10", '1', x"fffffffe", '1', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),
+    (x"0040056f", "-----", "-----", "01010", "10", '1', x"00000004", '1', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),
+    (x"ffdff7ef", "-----", "-----", "01111", "10", '1', x"fffffffc", '1', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),
+    (x"7ffff8ef", "-----", "-----", "10001", "10", '1', x"000ffffe", '1', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),
+    (x"80000b6f", "-----", "-----", "10110", "10", '1', x"fff00000", '1', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),
+
+    (x"00000067", "00000", "-----", "00000", "10", '1', x"00000000", '0', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),  -- JALR
+    (x"00408367", "00001", "-----", "00110", "10", '1', x"00000004", '0', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),
+    (x"ffc103e7", "00010", "-----", "00111", "10", '1', x"fffffffc", '0', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),
+    (x"3e8205e7", "00100", "-----", "01011", "10", '1', x"000003e8", '0', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),
+    (x"c18786e7", "01111", "-----", "01101", "10", '1', x"fffffc18", '0', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),
+    (x"7ff88e67", "10001", "-----", "11100", "10", '1', x"000007ff", '0', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),
+    (x"801f8ee7", "11111", "-----", "11101", "10", '1', x"fffff801", '0', '1', "00000", "---", '0', '1', "----", '-', '0', '0'),
+
+    (x"000f8063", "11111", "00000", "-----", "--", '0', x"00000000", '1', '1', "00000", "000", '1', '0', "----", '-', '0', '0'),  -- BEQ
+    (x"00180163", "10000", "00001", "-----", "--", '0', x"00000002", '1', '1', "00000", "000", '1', '0', "----", '-', '0', '0'),
+    (x"fe240ee3", "01000", "00010", "-----", "--", '0', x"fffffffc", '1', '1', "00000", "000", '1', '0', "----", '-', '0', '0'),
+    (x"004200e3", "00100", "00100", "-----", "--", '0', x"00000800", '1', '1', "00000", "000", '1', '0', "----", '-', '0', '0'),
+    (x"00810f63", "00010", "01000", "-----", "--", '0', x"0000001e", '1', '1', "00000", "000", '1', '0', "----", '-', '0', '0'),
+    (x"81008063", "00001", "10000", "-----", "--", '0', x"fffff000", '1', '1', "00000", "000", '1', '0', "----", '-', '0', '0'),
+    (x"7ff00063", "00000", "11111", "-----", "--", '0', x"000007e0", '1', '1', "00000", "000", '1', '0', "----", '-', '0', '0'),
+    (x"00001a63", "00000", "00000", "-----", "--", '0', x"00000014", '1', '1', "00000", "001", '1', '0', "----", '-', '0', '0'),  -- BNE
+    (x"00004863", "00000", "00000", "-----", "--", '0', x"00000010", '1', '1', "00000", "100", '1', '0', "----", '-', '0', '0'),  -- BLT
+    (x"00005663", "00000", "00000", "-----", "--", '0', x"0000000c", '1', '1', "00000", "101", '1', '0', "----", '-', '0', '0'),  -- BGE
+    (x"00006463", "00000", "00000", "-----", "--", '0', x"00000008", '1', '1', "00000", "110", '1', '0', "----", '-', '0', '0'),  -- BLTU
+    (x"00007263", "00000", "00000", "-----", "--", '0', x"00000004", '1', '1', "00000", "111", '1', '0', "----", '-', '0', '0'),  -- BGEU
+
+    (x"00080083", "10000", "-----", "00001", "01", '1', x"00000000", '0', '1', "00000", "---", '0', '0', "0001", '0', '1', '0'),  -- LB
+    (x"00441103", "01000", "-----", "00010", "01", '1', x"00000004", '0', '1', "00000", "---", '0', '0', "0011", '0', '1', '0'),  -- LH
+    (x"ffc22203", "00100", "-----", "00100", "01", '1', x"fffffffc", '0', '1', "00000", "---", '0', '0', "1111", '0', '1', '0'),  -- LW
+    (x"7ff14403", "00010", "-----", "01000", "01", '1', x"000007ff", '0', '1', "00000", "---", '0', '0', "0001", '1', '1', '0'),  -- LBU
+    (x"8010d803", "00001", "-----", "10000", "01", '1', x"fffff801", '0', '1', "00000", "---", '0', '0', "0011", '1', '1', '0'),  -- LHU
+
+    (x"00b38023", "00111", "01011", "-----", "--", '0', x"00000000", '0', '1', "00000", "---", '0', '0', "0001", '-', '0', '1'),  -- SB
+    (x"01761223", "01100", "10111", "-----", "--", '0', x"00000004", '0', '1', "00000", "---", '0', '0', "0011", '-', '0', '1'),  -- SH
+    (x"ff5f2e23", "11110", "10101", "-----", "--", '0', x"fffffffc", '0', '1', "00000", "---", '0', '0', "1111", '-', '0', '1'),  -- SW
+
+    (x"00008013", "00001", "-----", "00000", "00", '1', x"00000000", '0', '1', "00000", "---", '0', '0', "----", '-', '0', '0'),  -- ADDI
+    (x"0000a013", "00001", "-----", "00000", "00", '1', x"00000000", '0', '1', "00010", "---", '0', '0', "----", '-', '0', '0'),  -- SLTI
+    (x"0000b013", "00001", "-----", "00000", "00", '1', x"00000000", '0', '1', "00011", "---", '0', '0', "----", '-', '0', '0'),  -- SLTIU
+    (x"0000c013", "00001", "-----", "00000", "00", '1', x"00000000", '0', '1', "00100", "---", '0', '0', "----", '-', '0', '0'),  -- XORI
+    (x"0000e013", "00001", "-----", "00000", "00", '1', x"00000000", '0', '1', "00110", "---", '0', '0', "----", '-', '0', '0'),  -- ORI
+    (x"0000f013", "00001", "-----", "00000", "00", '1', x"00000000", '0', '1', "00111", "---", '0', '0', "----", '-', '0', '0'),  -- ANDI
+
+    (x"00109013", "00001", "-----", "00000", "00", '1', x"00000001", '0', '1', "00001", "---", '0', '0', "----", '-', '0', '0'),  -- SLLI
+    (x"0100d013", "00001", "-----", "00000", "00", '1', x"00000010", '0', '1', "00101", "---", '0', '0', "----", '-', '0', '0'),  -- SRLI
+    (x"41f0d013", "00001", "-----", "00000", "00", '1', x"0000041f", '0', '1', "01101", "---", '0', '0', "----", '-', '0', '0'),  -- SRAI
+
+    (x"00208033", "00001", "00010", "00000", "00", '1', x"--------", '0', '0', "00000", "---", '0', '0', "----", '-', '0', '0'),  -- ADD
+    (x"40208033", "00001", "00010", "00000", "00", '1', x"--------", '0', '0', "01000", "---", '0', '0', "----", '-', '0', '0'),  -- SUB
+    (x"00209033", "00001", "00010", "00000", "00", '1', x"--------", '0', '0', "00001", "---", '0', '0', "----", '-', '0', '0'),  -- SLL
+    (x"0020a033", "00001", "00010", "00000", "00", '1', x"--------", '0', '0', "00010", "---", '0', '0', "----", '-', '0', '0'),  -- SLT
+    (x"0020b033", "00001", "00010", "00000", "00", '1', x"--------", '0', '0', "00011", "---", '0', '0', "----", '-', '0', '0'),  -- SLTU
+    (x"0020c033", "00001", "00010", "00000", "00", '1', x"--------", '0', '0', "00100", "---", '0', '0', "----", '-', '0', '0'),  -- XOR
+    (x"0020d033", "00001", "00010", "00000", "00", '1', x"--------", '0', '0', "00101", "---", '0', '0', "----", '-', '0', '0'),  -- SRL
+    (x"4020d033", "00001", "00010", "00000", "00", '1', x"--------", '0', '0', "01101", "---", '0', '0', "----", '-', '0', '0'),  -- SRA
+    (x"0020e033", "00001", "00010", "00000", "00", '1', x"--------", '0', '0', "00110", "---", '0', '0', "----", '-', '0', '0'),  -- OR
+    (x"0020f033", "00001", "00010", "00000", "00", '1', x"--------", '0', '0', "00111", "---", '0', '0', "----", '-', '0', '0')   -- AND
+  );
+
+
+  -- returns false if inputs are different, true if one ore both are don't care
+  function compare (in1, in2 : in std_logic) return boolean is
+  begin
+    return in1 = in2 or in1 = '-' or in2 = '-';
+  end function compare;
+
+  -- returns false if inputs are different, true if one ore both are don't care
+  function compare (in1, in2 : in std_logic_vector) return boolean is
+  begin
+    for i in in1'range loop
+      if(compare(in1(i), in2(i)) = false) then
+        return false;
+      end if;
+    end loop;
+    return true;
+  end function compare;
+
+  -- converts logic vector to hex string
+  function to_hstring (SLV : std_logic_vector) return string is
+    variable L : LINE;
+  begin
+    hwrite(L,SLV);
+    return L.all;
+  end function to_hstring;
+
+  function to_hstring (SLV : std_logic) return string is
+    variable L : LINE;
+  begin
+    write(L,SLV);
+    return L.all;
+  end function to_hstring;
+
+
+  component decoder is
+    port (
+      i_clk         : in  std_logic;
+      i_ce          : in  std_logic;
+      i_opcode      : in  std_logic_vector(31 downto 0);
+      o_rs1         : out std_logic_vector( 4 downto 0);
+      o_rs2         : out std_logic_vector( 4 downto 0);
+      o_rd          : out std_logic_vector( 4 downto 0);
+      o_reg_mux     : out std_logic_vector( 1 downto 0);
+      o_reg_wb      : out std_logic;
+      o_imm         : out std_logic_vector(31 downto 0);
+      o_alu_in1_mux : out std_logic;
+      o_alu_in2_mux : out std_logic;
+      o_alu_func    : out std_logic_vector(4 downto 0);
+      o_branch_func : out std_logic_vector(2 downto 0);
+      o_branch      : out std_logic;
+      o_jump        : out std_logic;
+      o_byte_enable : out std_logic_vector(3 downto 0);
+      o_load_unsign : out std_logic;
+      o_load        : out std_logic;
+      o_store       : out std_logic
+    );
+  end component decoder;
+
+begin
+
+
+  -- Clock generation
+  p_clk : process
+  begin
+    wait for CLK_P * 0.5;
+    r_clk <= not r_clk;
+  end process p_clk;
+
+
+  -- Apply test patterns and assert on results
+  p_sim : process
+  begin
+
+    for i in patterns'range loop
+      r_ce    <= '1';
+      r_opcode <= patterns(i).opcode;
+      wait until rising_edge(r_clk);
+      r_ce    <= '0';
+      wait until rising_edge(r_clk);
+      assert compare(w_rs1          , patterns(i).rs1        ) report "Failed test " & integer'image(i) & " RS1 = "         & to_hstring(w_rs1        ) & " EXPECTED: " & to_hstring(patterns(i).rs1        ) severity failure;
+      assert compare(w_rs2          , patterns(i).rs2        ) report "Failed test " & integer'image(i) & " RS2 = "         & to_hstring(w_rs2        ) & " EXPECTED: " & to_hstring(patterns(i).rs2        ) severity failure;
+      assert compare(w_rd           , patterns(i).rd         ) report "Failed test " & integer'image(i) & " RD = "          & to_hstring(w_rd         ) & " EXPECTED: " & to_hstring(patterns(i).rd         ) severity failure;
+      assert compare(w_reg_mux      , patterns(i).reg_mux    ) report "Failed test " & integer'image(i) & " REG_MUX = "     & to_hstring(w_reg_mux    ) & " EXPECTED: " & to_hstring(patterns(i).reg_mux    ) severity failure;
+      assert compare(w_reg_wb       , patterns(i).reg_wb     ) report "Failed test " & integer'image(i) & " REG_WB = "      & to_hstring(w_reg_wb     ) & " EXPECTED: " & to_hstring(patterns(i).reg_wb     ) severity failure;
+      assert compare(w_imm          , patterns(i).imm        ) report "Failed test " & integer'image(i) & " IMM = "         & to_hstring(w_imm        ) & " EXPECTED: " & to_hstring(patterns(i).imm        ) severity failure;
+      assert compare(w_alu_in1_mux  , patterns(i).alu_in1_mux) report "Failed test " & integer'image(i) & " ALU_IN1_MUX = " & to_hstring(w_alu_in1_mux) & " EXPECTED: " & to_hstring(patterns(i).alu_in1_mux) severity failure;
+      assert compare(w_alu_in2_mux  , patterns(i).alu_in2_mux) report "Failed test " & integer'image(i) & " ALU_IN2_MUX = " & to_hstring(w_alu_in2_mux) & " EXPECTED: " & to_hstring(patterns(i).alu_in2_mux) severity failure;
+      assert compare(w_alu_func     , patterns(i).alu_func   ) report "Failed test " & integer'image(i) & " ALU_FUNC = "    & to_hstring(w_alu_func   ) & " EXPECTED: " & to_hstring(patterns(i).alu_func   ) severity failure;
+      assert compare(w_branch_func  , patterns(i).branch_func) report "Failed test " & integer'image(i) & " BRANCH_FUNC = " & to_hstring(w_branch_func) & " EXPECTED: " & to_hstring(patterns(i).branch_func) severity failure;
+      assert compare(w_branch       , patterns(i).branch     ) report "Failed test " & integer'image(i) & " BRANCH = "      & to_hstring(w_branch     ) & " EXPECTED: " & to_hstring(patterns(i).branch     ) severity failure;
+      assert compare(w_jump         , patterns(i).jump       ) report "Failed test " & integer'image(i) & " JUMP = "        & to_hstring(w_jump       ) & " EXPECTED: " & to_hstring(patterns(i).jump       ) severity failure;
+      assert compare(w_byte_enable  , patterns(i).byte_enable) report "Failed test " & integer'image(i) & " BYTE_ENABLE = " & to_hstring(w_byte_enable) & " EXPECTED: " & to_hstring(patterns(i).byte_enable) severity failure;
+      assert compare(w_load_unsign  , patterns(i).load_unsign) report "Failed test " & integer'image(i) & " LOAD_UNSIGN = " & to_hstring(w_load_unsign) & " EXPECTED: " & to_hstring(patterns(i).load_unsign) severity failure;
+      assert compare(w_load         , patterns(i).load       ) report "Failed test " & integer'image(i) & " LOAD = "        & to_hstring(w_load       ) & " EXPECTED: " & to_hstring(patterns(i).load       ) severity failure;
+      assert compare(w_store        , patterns(i).store      ) report "Failed test " & integer'image(i) & " STORE = "       & to_hstring(w_store      ) & " EXPECTED: " & to_hstring(patterns(i).store      ) severity failure;
+    end loop;
+
+    -- End simulation
+    finish;
+
+  end process p_sim;
+
+
+  dut : decoder
+    port map (
+        i_clk         => r_clk,
+        i_ce          => r_ce,
+        i_opcode      => r_opcode,
+        o_rs1         => w_rs1,
+        o_rs2         => w_rs2,
+        o_rd          => w_rd,
+        o_reg_mux     => w_reg_mux,
+        o_reg_wb      => w_reg_wb,
+        o_imm         => w_imm,
+        o_alu_in1_mux => w_alu_in1_mux,
+        o_alu_in2_mux => w_alu_in2_mux,
+        o_alu_func    => w_alu_func,
+        o_branch_func => w_branch_func,
+        o_branch      => w_branch,
+        o_jump        => w_jump,
+        o_byte_enable => w_byte_enable,
+        o_load_unsign => w_load_unsign,
+        o_load        => w_load,
+        o_store       => w_store
+    );
+
+end architecture behave;
